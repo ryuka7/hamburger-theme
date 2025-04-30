451045b03ccbad77fff33df2424c936d4fc8e0b6
ryuma <k-1rise.ryuma@au.com>
2025-04-30 19:10:43 +0900

    フォルダ整理完了

--- template-parts/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="c-footer">
-    <div class="c-footer__text">
-        <a href="">ショップ情報</a>｜<a href="">ヒストリー</a>
-        <small>Copyright:Raise Tech</small>
-    </div>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
