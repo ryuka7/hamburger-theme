48966d2e80c3593c393e5581df760ebe5cc7279f
ryuma <k-1rise.ryuma@au.com>
2025-05-02 11:24:17 +0900

    サブモジュール解除して中身を通常フォルダとして追加

---
+++ app/public/wp-content/themes/hamburger-theme/footer.php
@@ -0,0 +1,10 @@
+<footer class="c-footer">
+<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/footer-blown.png' ); ?>" alt="フッター画像">
+    <div class="c-footer__text">
+        <a href="">ショップ情報</a>｜<a href="">ヒストリー</a>
+        <small>Copyright:Raise Tech</small>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
